6469e1bf65316f165a1988575ab0fcd0319a47a5
wijayaerick <wijaya.erick52@gmail.com>
2018-05-16 13:40:30 +0700

    fps

--- OpenGL/UAS/src/main.cpp
+++ OpenGL/UAS/src/main.cpp
@@ -1208,8 +1208,20 @@ int main( )
     glBufferData(GL_ARRAY_BUFFER, MAX_SMOKE_PARTICLES * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
 
     // Game loop
+    double lastTime = glfwGetTime();
+    int nbFrames = 0;
     while( !glfwWindowShouldClose( window ) )
     {
+     // Measure speed
+        double currentTime = glfwGetTime();
+        nbFrames++;
+        if ( currentTime - lastTime >= 1.0 ){ // If last prinf() was more than 1 sec ago
+            // printf and reset timer
+            printf("%f ms/frame\n", 1000.0/double(nbFrames));
+            nbFrames = 0;
+            lastTime += 1.0;
+        }
+
         // Set frame time
         GLfloat currentFrame = glfwGetTime( );
         deltaTime = currentFrame - lastFrame;
